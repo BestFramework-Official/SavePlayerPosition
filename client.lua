87857d20c660e228b4479e6f1c100dc84ee1405b
BestFramework-Official <83039997+BestFramework-Official@users.noreply.github.com>
2021-04-22 23:43:17 +0200

    Updated client.lua
    Updated client.lua

---
+++ client.lua
@@ -0,0 +1,30 @@
+AddEventHandler('playerSpawned', function()
+    TriggerServerEvent('FW:SpawnPlayer')
+end)
+
+-- Setup player position --
+RegisterNetEvent('FW:lastPosition')
+AddEventHandler('FW:lastPosition', function(PosX, PosY, PosZ)
+    Citizen.Wait(1)
+
+    local defaulModel = GetHashKey('mp_m_freemode_01')
+    RequestModel(defaulModel)
+    while not HasModelLoaded(defaulModel) do
+        Citizen.Wait(1)
+    end
+    SetPlayerModel(PlayerId(), defaulModel)
+    SetPedDefaultComponentVariation(PlayerPedId())
+    SetModelAsNoLongerNeeded(defaulModel)
+
+    SetEntityCoords(GetPlayerPed(-1), PosX, PosY, PosZ, 1, 0, 0, 1)
+end)
+
+-- Update player position and Save to database
+Citizen.CreateThread(function()
+    while true do 
+        Citizen.Wait(5000)
+
+        lastX, lastY, lastZ = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
+        TriggerServerEvent('FW:SavePlayerPosition', lastX, lastY, lastZ)
+    end
+end)
